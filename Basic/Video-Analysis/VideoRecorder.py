b3852af19ed5c7874cf54c1c671df132f7380b15
asadullah-dal17 <asadullah.dal17@gmail.com>
2021-06-25 08:34:21 +0500

    completed Video Analysis

--- Basic/Video-Analysis/VideoRecorder.py
+++ Basic/Video-Analysis/VideoRecorder.py
@@ -2,22 +2,26 @@
 import cv2 as cv 
 
 cap = cv.VideoCapture('carVideo.mp4')
-width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
+width = cap.get(cv.CAP_PROP_FRAME_WIDTH)/3
+height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)/3
+FPS = cap.get(cv.CAP_PROP_FPS)
 
-height 
-
-# video recording settings 
+# video recording paramters 
 fourcc = cv.VideoWriter_fourcc(*'XVID')
-Recorder = cv.VideoWriter('outputVideo.avi', fourcc, fps, (width, height))
+Recoder = cv.VideoWriter('false.avi', fourcc, FPS,(int(width), int(height))) # keep the same size, of resize and video recorder 
 
 while True:
     ret, frame = cap.read()
     if ret is False:
         break
-    cv.imshow('frame', frame)
+    # cv.imshow('frame', frame)
+    resized = cv.resize(frame, (int(width), int(height)), interpolation=cv.INTER_AREA)
+    Recoder.write(resized)
 
     key = cv.waitKey(1)
     if key ==ord('q'):
         break
 cv.destroyAllWindows()
+Recoder.release()
 cap.release()
+print('done')
